d53bc1dd7d50b68219c118c7f1005b67a5c20951
SiddhiDandare <84766846+SiddhiDandare@users.noreply.github.com>
2023-06-06 11:53:30 +0530

    Add files via upload

---
+++ pen_pouch.js
@@ -0,0 +1,30 @@
+Status = "";
+pen_pouch_image = "";
+
+function preload(){
+    pen_pouch_image = loadImage("pen_pouch.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(pen_pouch_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(pen_pouch_image,0,0,640,350);
+}
